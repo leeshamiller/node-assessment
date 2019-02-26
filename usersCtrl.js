cf0f6a1c801bceca5a17560e246e37d46c8092c3
Todd Rasband <todd.rasband@gmail.com>
2019-02-26 00:35:21 -0700

    notes

--- usersCtrl.js
+++ usersCtrl.js
@@ -20,6 +20,7 @@ module.exports = {
     },
     getUserId: (req, res) => {
         let { userId } = req.params
+        // the ternary here is not really necessary, as the === resolves into true and false already
         let i = userData.findIndex((user) => +user.id === +userId ? true : false)
         if (i !== -1 && +userId === +userData[i].id) {
             res.status(200).send(userData[i])
@@ -43,8 +44,10 @@ module.exports = {
     },
     updateUserId: (req, res) => {
         // console.log(req.params.userId)
+        // Here you should just overwrite userData with userData.
         let updatedUserData = userData.map((user, i) => {
             if (+req.params.userId === +user.id) {
+                // you may want to try using the spread operator here to save you a lot of typing
                 user = {
                     id: +req.params.userId,
                     first_name: req.body.first_name,
@@ -56,9 +59,13 @@ module.exports = {
                     city: req.body.city,
                     state: req.body.state,
                     type: req.body.type,
+                    // this would actualy create an array of one item, that one item being an array, again, spread operator could be good here but really just dropping the brackets would be best
                     favorites: [req.body.favorites]
                 }
+                // this line is is not the best idea. you are removing the item from the array mid loop, then insert it back in.
+                // I also ends up not being needed, because you return the user on the next line and take advantage of the feature of the map.
                 userData.splice(i, 1, user)
+                // this is not needed because you do a return on the next line anyway
                 return user
             }
             return user
@@ -66,6 +73,8 @@ module.exports = {
         return res.status(200).send(updatedUserData)
     },
     addUser: (req, res) => {
+        // the ++ operator modifies the value you are using it on. so this would modify the property length, but the actual length would not reflect it.
+        // It is also not a good idea to base the index off the length of the array. You don't have an easy way to ensure the id is not repead. better to have an id varaible to track the last used id and incrment that.
         let id = userData.length++;
         let user = req.body
         user.id = id
@@ -75,6 +84,7 @@ module.exports = {
     },
     deleteUser: (req, res) => {
         let id = req.params.userId;
+        //same thing about the ternary
         let i = userData.findIndex((user) => +user.id === +id ? true : false)
         // console.log(userData.length)
         if (i !== -1) {
