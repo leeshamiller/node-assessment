117065f0f8a2860de772a6652364b17d5568cd96
Leesha Miller <leesha.miller11@gmail.com>
2019-02-26 08:23:41 -0700

    revised

--- usersCtrl.js
+++ usersCtrl.js
@@ -1,8 +1,7 @@
-const userData = require('./userData.json');
+let userData = require('./userData.json');
 
 module.exports = {
     getUser: (req, res) => {
-        // console.log(req.query)
         if (req.query.age) {
             let lessThanAge = userData.filter(user => user.age < req.query.age)
             res.status(200).send(lessThanAge)
@@ -20,13 +19,13 @@ module.exports = {
     },
     getUserId: (req, res) => {
         let { userId } = req.params
-        let i = userData.findIndex((user) => +user.id === +userId ? true : false)
+        let i = userData.findIndex((user) => +user.id === +userId)
         if (i !== -1 && +userId === +userData[i].id) {
             res.status(200).send(userData[i])
         } else {
             res.sendStatus(404)
         }
-        
+
     },
     getAdmin: (req, res) => {
         let admin = userData.filter(user => user.type === 'admin')
@@ -42,46 +41,37 @@ module.exports = {
         res.status(200).send(userTypeArr)
     },
     updateUserId: (req, res) => {
-        // console.log(req.params.userId)
-        let updatedUserData = userData.map((user, i) => {
+        userData.map((user, i) => {
             if (+req.params.userId === +user.id) {
-                user = {
-                    id: +req.params.userId,
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name,
-                    email: req.body.email,
-                    gender: req.body.gender,
-                    language: req.body.language,
-                    age: req.body.age,
-                    city: req.body.city,
-                    state: req.body.state,
-                    type: req.body.type,
-                    favorites: [req.body.favorites]
-                }
-                userData.splice(i, 1, user)
+                user = { ...userData, user }
                 return user
             }
             return user
         })
-        return res.status(200).send(updatedUserData)
+
+        return res.status(200).send(userData)
     },
     addUser: (req, res) => {
-        let id = userData.length++;
-        let user = req.body
-        user.id = id
-        userData.push(user)
-        console.log(user)
+        let idCount = 0;
+        for(let i = 0; i > userData.length; i++) {
+            if (userData[i].id > idCount) {
+                idCount = userData[i].id 
+            } else {
+                 idCount++
+                let user = req.body
+                user.id = idCount
+                userData.push(user)
+            }
+        }
+
         res.status(200).send(userData)
     },
     deleteUser: (req, res) => {
         let id = req.params.userId;
-        let i = userData.findIndex((user) => +user.id === +id ? true : false)
-        // console.log(userData.length)
+        let i = userData.findIndex((user) => +user.id === +id)
         if (i !== -1) {
             userData.splice(i, 1)
         }
-        // console.log(id, i)
-        // console.log(userData.length)
         res.status(200).send(userData)
     }
 }
